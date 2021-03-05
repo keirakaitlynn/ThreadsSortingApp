f0dd32fe018867442be3b45aadb3f6c44e6ac988
keira <keirakaitlynn@gmail.com>
2021-03-04 18:47:23 -0800

    Initial Commit.

---
+++ main.c
@@ -0,0 +1,38 @@
+/**
+ * project 2
+ */
+
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+//add your code here
+
+void *sorter(void *params);	/* thread that performs basic sorting algorithm */
+void *merger(void *params);	/* thread that performs merging of results */
+
+
+
+int main (int argc, const char * argv[])
+{
+	//Your code here
+}
+
+/**
+ * Sorting thread.
+ */
+
+void *sorter(void *params)
+{
+	//Your code here
+}
+
+/**
+ * Merge thread
+ */
+
+void *merger(void *params)
+{
+	//Your code here
+}
+
